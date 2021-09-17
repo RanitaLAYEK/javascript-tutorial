e2d5305c0d460e13204660849152a175179a1416
ranita <ranitalayek99@gmail.com>
2021-09-17 10:17:26 +0530

    ELEMENT

--- js/tut16.js
+++ js/tut16.js
@@ -2,3 +2,80 @@ console.log('welcome to  tut16');
 /*
 Element creation,removing,replace
 */
+//Add a classname to the li Element.
+
+// let element =document.createElement('li');
+// element.className ='childu1';
+// element.id='createLi';
+// element.setAttribute('title','mytitle');
+
+
+// //console.log(element);
+// //  How to implement in DOM
+
+// let ul=document.querySelector('ul.this');
+// ul.appendChild(element);
+// //append child to append child in DOM
+// element.innerText='Hello  is created by Ranita';
+// element.innerHTML='<b>Hello  is created by Ranita<b>';
+
+// //console.log(ul);
+// //console.log(element);
+
+// let text=document.createTextNode(' '+'stay safe');
+// element.appendChild(text);
+
+// //How to replace a element
+
+// let elem2=document.createElement("h3");
+// elem2.id='elem2';
+// elem2.className='elem2';
+
+// let tnode=document.createTextNode('you are in elem2');
+// elem2.appendChild(tnode);
+// element.replaceWith(elem2);
+
+// let myfai=document.getElementById('myul')
+// //myul.replaceChild(document.getElementById(new element in 1st position,old in 2nd position));
+
+// myfai.replaceChild(element,document.getElementById('full'));
+// //parentNode.replaceChild(newNode, currentSelectList);
+
+
+console.log('this is tut 16');
+let element = document.createElement('li');
+let text = document.createTextNode('I am a text node');
+element.appendChild(text)
+
+// Add a class name to the li element
+element.className = 'childul';
+element.id = 'createdLi';
+element.setAttribute('title', 'mytitle');
+// element.innerText = '<b>Hello this is created by harry</b>';
+// element.innerHTML = '<b>Hello this is created by harry</b>';
+
+let ul = document.querySelector('ul.this');
+ul.appendChild(element);
+// console.log(ul)
+// console.log(element)
+
+let elem2 = document.createElement('h3');
+elem2.id = 'elem2';
+elem2.className = 'elem2';
+let tnode = document.createTextNode('This is a created node for elem2');
+elem2.appendChild(tnode);
+
+element.replaceWith(elem2);
+let myul = document.getElementById('myul');
+myul.replaceChild(element, document.getElementById('fui'));
+myul.removeChild(document.getElementById('lui'));
+let pr = elem2.hasAttribute('href');
+elem2.removeAttribute('id');
+elem2.setAttribute('title', 'myelem2title');
+console.log(elem2, pr);
+
+// quick quiz
+// create a heading element with text as "Go to CodeWithHarry" and create an a tag outside it with href = "https://www.codewithharry.com"
+
+
+
