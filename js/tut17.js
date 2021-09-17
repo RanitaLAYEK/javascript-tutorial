423a6487f7b7f713a9eaafa2ee819d759a1ba5f9
ranita <ranitalayek99@gmail.com>
2021-09-17 12:32:53 +0530

    eventfire

---
+++ js/tut17.js
@@ -0,0 +1,7 @@
+console.log('welcome to tut17');
+
+//element grab & event fire.
+
+document.getElementById('heading').addEventListener('click',function () {
+    console.log('you are clicking in heading');
+})
