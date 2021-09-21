a43e4b87ae7e5b2212fc19f95d1c741f4fd1f602
ranita <ranitalayek99@gmail.com>
2021-09-21 19:50:22 +0530

    Examp

---
+++ js/tut21.js
@@ -0,0 +1,8 @@
+console.log('Tutorial 21 exercise JavaScript');
+
+/*
+You have to create a div and inject it into the page which contains a heading.
+whenever someone clicks on the div, it should be converted into an editable item. whenever user clicks away (blur). save the note into the local storage.
+
+*/
+
