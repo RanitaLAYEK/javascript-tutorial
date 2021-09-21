2bc4fb9944eb6d701a50246c76a64a461bba5e2f
ranita <ranitalayek99@gmail.com>
2021-09-21 19:51:14 +0530

    Examp

--- js/tut20.js
+++ js/tut20.js
@@ -1,1 +1,40 @@
 console.log('this is 20');
+
+//LOCAL STORAGE
+
+localStorage.setItem('Name','Rit');
+localStorage.setItem('Name2','prachi');
+
+let name= localStorage.getItem('Name');
+console.log(name);
+
+//to clear local storage
+
+//localstorage.clear;
+
+//clear a particular key
+localStorage.removeItem('Name');
+
+//Array adding
+
+let impArray = ['adrak', 'pyaz', 'bhindi'];
+
+// // Add a key-value pair inside local Storage
+// localStorage.setItem('Name', 'Harry');
+// localStorage.setItem('Name2', 'Rohan');
+// localStorage.setItem('Sabzi', JSON.stringify(impArray));
+
+// Clears the entire local storage
+// localStorage.clear();
+
+// Clear a particular key-value pair
+// localStorage.removeItem('Name2');
+
+// Retrieve an item from the local Storage
+let name = localStorage.getItem('Name');
+name = JSON.parse(localStorage.getItem('Sabzi'));
+console.log(name)
+
+// sessionStorage.setItem('sessionName', 'sHarry');
+// sessionStorage.setItem('sessionName2', 'sRohan');
+// sessionStorage.setItem('sessionSabzi', JSON.stringify(impArray));
