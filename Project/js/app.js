23ec515fe12a58137051620dd27fa69060cf8c21
ranita <ranitalayek99@gmail.com>
2021-09-29 21:24:30 +0530

    project

--- Project/js/app.js
+++ Project/js/app.js
@@ -6,12 +6,45 @@ if user add a note add it to the local storage
 
 */
 
-let btn=document.getElementById("addbtn");
-addbtn.addeventlistner('click',function(e)
-{
-    let addTxt=document.getElementById('addTxt');
-    let notesElement=localStorage.getItem('notes');
-    if(notes==null){//to check if any notes are present in local storage
-        notesObj=[];
-    }
+// let addBtn=document.getElementById("addBtn");
+// addBtn.addEventlistner("click",function(e)
+// {
+//     let addTxt=document.getElementById('addTxt');
+//     let notes=localStorage.getItem("notes");
+//     if(notes==null){//to check if any notes are present in local storage
+//         notesObj=[];
+//     }
+//     else{
+//         notesObj=JSON.parse(notes);
+//     }
+//     notesObj.push(addTxt.value);
+//     localstorage.setItem("notes",JSON.stringify(notesObj));
+//     addTxt.value="";
+//     console.log(notesObj);
+// })
+
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function(e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+console.log(notesObj);
+shownotes();
 })
+
+function shownotes(){
+    let notes=localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+      } else {
+        notesObj = JSON.parse(notes);
+      }
+}
