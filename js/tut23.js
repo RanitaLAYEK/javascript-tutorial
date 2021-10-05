b470318d51d5132ed96e3ed9b2f3a76648e87a7b
ranita <ranitalayek99@gmail.com>
2021-10-05 12:31:45 +0530

    abc

---
+++ js/tut23.js
@@ -0,0 +1,23 @@
+console.log('welcome to 23 ');
+
+let x=6;
+let y=3;
+
+let z;
+z=x+y;
+z=x-y;
+z=x*y;
+z=x/y;
+z=Math;
+z=Math.PI;
+z=Math.E;
+z=Math.round(5.4);
+z=Math.cell(5.6);
+z=Math.floor(5.3);
+z=Math.abs(5.3);
+z=Math.sqrt(53);
+z=Math.paw(5,3);
+z=Math.min(5,3,4,6);
+z=Math.max(3,5,8,9);
+z=Math.random();
+console.log(z);
